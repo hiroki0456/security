9ac9e23a107b59a0da75a4eb3c3cb034c6da0d57
hiroki0456 <volleyball0456@gmail.com>
2022-04-17 16:15:26 +0900

    add run method

--- netstat.go
+++ netstat.go
@@ -1,10 +1,77 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"os"
+	"os/exec"
+	"strings"
+)
 
-func execute(cmd string) {
-	cmd = strings.TrimSpace(cmd)
-	if cmd == "" {
-		return
+type NetCat struct {
+	buffer string
+}
+
+type NetCater interface {
+	run()
+	send()
+	listen()
+	handle()
+}
+
+var (
+	cmd_flag     bool
+	execute_flag string
+	listen_flag  bool
+	port_flag    int
+	target_flag  string
+	upload_flag  string
+)
+
+func init() {
+	flag.BoolVar(&cmd_flag, "c", true, "対話型シェルの初期化")
+	flag.StringVar(&execute_flag, "e", "", "指定のコマンドの実行")
+	flag.BoolVar(&listen_flag, "l", true, "通信待受モード")
+	flag.IntVar(&port_flag, "p", 5555, "ポート番号の指定")
+	flag.StringVar(&target_flag, "t", "192.168.1.203", "IPアドレスの指定")
+}
+
+func execute(cmdstr string) (string, error) {
+	cmdstr = strings.TrimSpace(cmdstr)
+	splitedcmd := strings.Split(cmdstr, " ")
+	if cmdstr == "" {
+		return "", errors.New("must input command")
 	}
+
+	output, err := exec.Command(splitedcmd[0], splitedcmd[1:]...).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func (nc *NetCat) run() {
+	if listen_flag {
+		nc.listen()
+	} else {
+		nc.send()
+	}
+}
+
+func (nc *NetCat) send() {
+
+}
+
+func main() {
+	var buffer string
+	if listen_flag {
+		buffer = ""
+	} else {
+		buffer = bufio.NewScanner(os.Stdin).Text()
+	}
+	nc := NetCat{
+		buffer: buffer,
+	}
+	execute("ls -al")
 }
